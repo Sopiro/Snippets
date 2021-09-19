beb359519c4a2c296af2638e2b5dd88a6ed69670
Sopiro <movindeep@gmail.com>
2021-09-20 03:03:28 +0900

    Add some math-related functions.

--- js/maths.js
+++ js/maths.js
@@ -1,5 +1,23 @@
 'use strict'
 
+function map(v, a, b, min, max)
+{
+    const per = v / (b - a);
+    return lerp(min, max, per);
+}
+
+function lerp(a, b, per)
+{
+    return a + (b - a) * per;
+}
+
+function clamp(v, min, max)
+{
+    if (v < min) return min;
+    else if (v >= max) return max;
+    else return v;
+}
+
 // Return line intersection
 function getIntersect(x1, y1, x2, y2, x3, y3, x4, y4)
 {
