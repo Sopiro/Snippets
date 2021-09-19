b63c65091cdf1dd2ac3ff3ecf17c5782bb7205c6
Sopiro <movindeep@gmail.com>
2021-09-20 03:01:52 +0900

    Add quick select

--- cpp/Searchings.h
+++ cpp/Searchings.h
@@ -6,6 +6,54 @@
 
 using namespace std;
 
+int _quick_select(vector<int> v, int n, int left, int right)
+{
+    int mid = (right + left) / 2;
+    int pivot = v[mid];
+
+    std::swap(v[(right + left) / 2], v[right]);
+
+    int lp = left;
+    int rp = right - 1;
+
+    while (rp >= lp)
+    {
+        while (v[lp] < pivot)
+            lp++;
+        while (v[rp] >= pivot)
+            rp--;
+
+        if (lp > rp)
+            break;
+        else
+            std::swap(v[lp], v[rp]);
+    }
+
+    std::swap(v[lp], v[right]);
+
+    // cout << mid << "[" << pivot << "] " << join(v, " ") << endl;
+
+    if (lp == (n - 1))
+        return v[lp];
+    else if (lp < n - 1)
+        return _quick_select(v, n, lp + 1, right);
+    else
+        return _quick_select(v, n, left, rp);
+}
+
+// Find the n-th smallist number in unsorted array
+int quick_select(vector<int> &v, int n)
+{
+    srand(time(NULL));
+
+    if (n < 1)
+        n = 1;
+    if (n > v.size())
+        n = v.size();
+
+    return _quick_select(v, n, 0, v.size() - 1);
+}
+
 int _binarySearch(vector<int> &v, int value, int left, int right)
 {
     int mid = (left + right) / 2;
@@ -17,7 +65,7 @@ int _binarySearch(vector<int> &v, int value, int left, int right)
                                                   : _binarySearch(v, value, left, mid - 1);
 }
 
-int binarySearch(vector<int> v, int value)
+int binarySearch(vector<int> &v, int value)
 {
     return _binarySearch(v, value, 0, v.size() - 1);
 }
--- cpp/Test.cpp
+++ cpp/Test.cpp
@@ -5,20 +5,22 @@
 
 int main(void)
 {
-    vector<int> v = {1, 3, 5, 2, 4, 8, 6, 7, 9};
+    vector<int> v = {1, 9};
     cout << "original: " << join(v, " ") << endl;
-    cout << "isHeap: " << isHeap(v) << endl;
+    // cout << "isHeap: " << isHeap(v) << endl;
 
-    heapify(v);
-    cout << "result:" << join(v, " ") << endl;
-    cout << "isHeap: " << isHeap(v) << endl;
+    // heapify(v);
+    // cout << "result:" << join(v, " ") << endl;
+    // cout << "isHeap: " << isHeap(v) << endl;
 
-    cout << popHeap(v) << endl;
-    cout << "result:" << join(v, " ") << endl;
-    insertHeap(v, 1001);
-    cout << "result:" << join(v, " ") << endl;
-    heapSort(v);
-    cout << "result:" << join(v, " ") << endl;
+    // cout << popHeap(v) << endl;
+    // cout << "result:" << join(v, " ") << endl;
+    // insertHeap(v, 1001);
+    // cout << "result:" << join(v, " ") << endl;
+    // heapSort(v);
+    // cout << "result:" << join(v, " ") << endl;
+
+    cout << quick_select(v, 1) << endl;
 
     return 0;
 }
